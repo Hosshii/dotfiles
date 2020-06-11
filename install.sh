f85f10ae41d1b026b1e10e2ab4f8eef443441914
Hosshii <sao_heath6147.wistre@icloud.com>
2020-06-11 18:09:50 +0900

    fix

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@ WORKDIR=$(pwd)/"$DOTPATH"
 #コマンド確認
 #gitコマンドが使えるか
 if type git >/dev/null 2>&1; then
-   $( git clone --recursive "$GITHUB_URL""$DOTPATH"".git")
+    $(git clone --recursive "$GITHUB_URL""$DOTPATH"".git")
 
 #使えなかったらcurlかwgetを探す
-elif type curl ||type wget >/dev/null 2>&1; then
+elif type curl || type wget >/dev/null 2>&1; then
     tarball="url" #TODO
     if type curl >/dev/null 2>&1; then
         curl -LO "$tarball"
-        
+
     elif type wget >/dev/null 2>&1; then
         wget -O - "$tarball"
 
-    fi |tar zxv
+    fi | tar zxv
     mv -f dotfiles-master "$DOTPATH"
 
 else
@@ -33,8 +33,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-for f in .??*
-do
+for f in .??*; do
     [ "$f" = ".git" ] && continue
     ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
