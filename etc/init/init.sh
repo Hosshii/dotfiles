09dcd35a8fa56bc39663129edd6508c4402a52ea
Hosshii <sao_heath6147.wistre@icloud.com>
2020-07-04 00:15:46 +0900

    fix path

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(
+SCRIPT_DIR=$(dirname $(dirname $(
     cd $(dirname $0)
     pwd
-))
+)))
+echo $SCRIPT_DIR
 
 if [ ! -d "${HOME}/.dotfiles" ]; then
     ln -sfnv "$SCRIPT_DIR" "${HOME}/.dotfiles"
