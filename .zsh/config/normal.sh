5da3670bcd43b99d3687ed598a130f4fdd6df011
Hosshii <sao_heath6147.wistre@icloud.com>
2020-12-21 22:12:31 +0900

    dont use ctrl^j because vscode's go extension cause bug

--- .zsh/config/normal.sh
+++ .zsh/config/normal.sh
@@ -42,63 +42,63 @@ function fzf-src() {
 zle -N fzf-src
 bindkey '^g' fzf-src
 
-# 今いるディレクトリの検索
-function fzf-path() {
-
-  # 最後が/で終わるかどうか(ディレクトリだったらctr＋jでその下の
-  # ディレクトリ を保管できるように
-  local tmp=${LBUFFER: -1}
-  if [ "$tmp" = "/" ]; then
-    # チルダは手動で展開する
-    # tmpにディレクトリ 名と思わし着物を格納
-    # 例えば cd /usr/bin/ でこのショートカットを使った場合
-    # tmpには /usr/bin/が入る
-    tmp=$(echo $LBUFFER | awk '{print $NF}' | sed -e "s#~#$HOME#")
-    # tmpがディレクトリかどうか確認
-    if [ -d $tmp ]; then
-      # ディレクトリならそこを検索対象にする
-      local search=$tmp
-    fi
-  fi
-
-  # 表示するファイルパス
-  # searchがあるなら、そのディレクトリ以下をfdコマンドで探す。
-  # そうじゃない時は、カレンとディレクトリ
-  # previewには、選択しているものがディレクトリならexaでツリー状に表示(カラーにするとうまくいかなかったのでしてない)
-  # ファイルならbatコマンドで表示している
-  #local filepath="$(fd . $search -H -E ".git" -c always | fzf --ansi --prompt 'PATH>' --preview 'if [ -d {} ];then exa   -T --git-ignore  {}|head -200;elif [ -f {} ];then bat --color=always --style=header,grid --line-range :100 {} | head -100;fi ')"
-  local filepath="$(fd . $search -H -E ".git" -c always | fzf --ansi --prompt 'PATH>' --preview 'if [ -d {} ];then tree  {}|head -200;elif [ -f {} ];then bat --color=always --style=header,grid --line-range :100 {} | head -100;fi ')"
-  [ -z "$filepath" ] && return
-
-  # filepathは$searchのところも含まれているので、そこを除く。ディレクトリなら最後に/をつける。
-  if [ ! -z $search ]; then
-    if [ -d $filepath ]; then
-      filepath="$(echo $filepath | sed -e "s#$search##")/"
-    elif [ -f $filepath ]; then
-      filepath=$(echo $filepath | sed -e "s#$search##")
-    fi
-  fi
-
-  # プロンプトで何か入力したあとにこのショートカットを使ったなら、そこに選んだファイルパスを付け加える。
-  if [ -n "$LBUFFER" ]; then
-    BUFFER="$LBUFFER$filepath"
-  else
-    # 何もない状態でディレクトリなら移動する。
-    if [ -d "$filepath" ]; then
-      BUFFER="cd $filepath"
-      zle accept-line
-    # ファイルなら$EDITORで開く。
-    elif [ -f "$filepath" ]; then
-      BUFFER="$EDITOR $filepath"
-      zle accept-line
-    fi
-  fi
-  zle clear-screen
-  CURSOR=$#BUFFER
-}
-
-zle -N fzf-path
-bindkey '^j' fzf-path # Ctrl+f で起動
+## 今いるディレクトリの検索
+#function fzf-path() {
+#
+#  # 最後が/で終わるかどうか(ディレクトリだったらctr＋jでその下の
+#  # ディレクトリ を保管できるように
+#  local tmp=${LBUFFER: -1}
+#  if [ "$tmp" = "/" ]; then
+#    # チルダは手動で展開する
+#    # tmpにディレクトリ 名と思わし着物を格納
+#    # 例えば cd /usr/bin/ でこのショートカットを使った場合
+#    # tmpには /usr/bin/が入る
+#    tmp=$(echo $LBUFFER | awk '{print $NF}' | sed -e "s#~#$HOME#")
+#    # tmpがディレクトリかどうか確認
+#    if [ -d $tmp ]; then
+#      # ディレクトリならそこを検索対象にする
+#      local search=$tmp
+#    fi
+#  fi
+#
+#  # 表示するファイルパス
+#  # searchがあるなら、そのディレクトリ以下をfdコマンドで探す。
+#  # そうじゃない時は、カレンとディレクトリ
+#  # previewには、選択しているものがディレクトリならexaでツリー状に表示(カラーにするとうまくいかなかったのでしてない)
+#  # ファイルならbatコマンドで表示している
+#  #local filepath="$(fd . $search -H -E ".git" -c always | fzf --ansi --prompt 'PATH>' --preview 'if [ -d {} ];then exa   -T --git-ignore  {}|head -200;elif [ -f {} ];then bat --color=always --style=header,grid --line-range :100 {} | head -100;fi ')"
+#  local filepath="$(fd . $search -H -E ".git" -c always | fzf --ansi --prompt 'PATH>' --preview 'if [ -d {} ];then tree  {}|head -200;elif [ -f {} ];then bat --color=always --style=header,grid --line-range :100 {} | head -100;fi ')"
+#  [ -z "$filepath" ] && return
+#
+#  # filepathは$searchのところも含まれているので、そこを除く。ディレクトリなら最後に/をつける。
+#  if [ ! -z $search ]; then
+#    if [ -d $filepath ]; then
+#      filepath="$(echo $filepath | sed -e "s#$search##")/"
+#    elif [ -f $filepath ]; then
+#      filepath=$(echo $filepath | sed -e "s#$search##")
+#    fi
+#  fi
+#
+#  # プロンプトで何か入力したあとにこのショートカットを使ったなら、そこに選んだファイルパスを付け加える。
+#  if [ -n "$LBUFFER" ]; then
+#    BUFFER="$LBUFFER$filepath"
+#  else
+#    # 何もない状態でディレクトリなら移動する。
+#    if [ -d "$filepath" ]; then
+#      BUFFER="cd $filepath"
+#      zle accept-line
+#    # ファイルなら$EDITORで開く。
+#    elif [ -f "$filepath" ]; then
+#      BUFFER="$EDITOR $filepath"
+#      zle accept-line
+#    fi
+#  fi
+#  zle clear-screen
+#  CURSOR=$#BUFFER
+#}
+#
+#zle -N fzf-path
+#bindkey '^j' fzf-path # Ctrl+f で起動
 
 # ctrl+nで今までに行ったことにあるディレクトリ をサーチ。
 fzf-z-search() {
