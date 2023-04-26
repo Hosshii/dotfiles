197b36a418e76776cd378b7b1434314fea6b8876
Hosshii <sao_heath6147.wistre@icloud.com>
2023-04-26 19:56:58 +0900

    update dein

--- etc/init/vim/init.sh
+++ etc/init/vim/init.sh
@@ -4,7 +4,5 @@ if [ ! -d "${HOME}/.cache/dein" ]; then
     echo "set up vim dein"
     echo "install dein"
     echo ""
-    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >installer.sh
-    sh ./installer.sh ~/.cache/dein
-    rm ./installer.sh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh)"
 fi
