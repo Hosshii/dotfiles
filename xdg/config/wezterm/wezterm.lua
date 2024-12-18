39cfffeb641002ab33253457f62a955bd5de0cbb
Hosshii <sao_heath6147.wistre@icloud.com>
2024-12-18 22:57:11 +0900

    add wezterm config

--- xdg/config/wezterm/wezterm.lua
+++ xdg/config/wezterm/wezterm.lua
@@ -1,10 +1,19 @@
 local wezterm = require 'wezterm';
 
+-- 常にフルスクリーンにする
+-- https://github.com/wez/wezterm/issues/284
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 return {
-  font = wezterm.font("HackGen35 Console NF"), -- 自分の好きなフォントいれる
-  use_ime = true, -- wezは日本人じゃないのでこれがないとIME動かない
+  font = wezterm.font("HackGen35 Console NF"),
+  use_ime = true, 
   font_size = 13.0,
---   color_scheme = "OneHalfDark", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
+  color_scheme = "Solarized Light (Gogh)", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   keys = {
@@ -13,6 +22,8 @@ return {
     -- ctrl+'|'で左右で分割
     {key="t",mods="CTRL",action=wezterm.action.SplitHorizontal({domain="CurrentPaneDomain"})},
     -- ctrl+'w'でpaneを閉じる
-    {key="w",mods="CTRL",action=wezterm.action.CloseCurrentPane{confirm=false}}
+    {key="w",mods="CTRL",action=wezterm.action.CloseCurrentPane{confirm=false}},
+    -- full screen 切り替え
+    {key = 'h',mods="CTRL",action = wezterm.action.ToggleFullScreen},
   }
 }
