715d3f81ab6a2b9e7096a167b9e64b9f1ec82c47
Hosshii <sao_heath6147.wistre@icloud.com>
2026-01-24 23:15:55 +0900

    Nix fmt

--- nix/flake.nix
+++ nix/flake.nix
@@ -16,74 +16,75 @@
   };
 
   outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }:
-  let
-    # システムアーキテクチャ（Apple Silicon）
-    system = "aarch64-darwin";
-    hostname = "andouhanshirous-MacBook-Air";
-    username = "andouhanshirou";
-    homedir = /Users/${username};
+    let
+      # システムアーキテクチャ（Apple Silicon）
+      system = "aarch64-darwin";
+      hostname = "andouhanshirous-MacBook-Air";
+      username = "andouhanshirou";
+      homedir = /Users/${username};
 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # installed via determinate
-      nix.enable = false;
-      system = {
-        # Set Git commit hash for darwin-version.
-        configurationRevision = self.rev or self.dirtyRev or null;
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        stateVersion = 6;
+      pkgs = import nixpkgs {
+        inherit system;
       };
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = system;
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages =
+          [
+            pkgs.vim
+          ];
 
-      programs.zsh.shellInit = ''
-        # Set up Nix only on SSH connections
-        # See: https://github.com/DeterminateSystems/nix-installer/pull/714
-        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ] && [ -n "''${SSH_CONNECTION:-}" ] && [ "''${SHLVL:-0}" -eq 1 ]; then
-            . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-        fi
-        # End Nix
-        export ZDOTDIR="$HOME"/.config/zsh
-      '';
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration
-        home-manager.darwinModules.home-manager
-        {
-          users.users.andouhanshirou.home = homedir;
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users."${username}" = {
-              home = {
-                username = username;
-                homeDirectory = homedir;
-                # home-manager のステートバージョン
-                stateVersion = "25.11";
+        # installed via determinate
+        nix.enable = false;
+        system = {
+          # Set Git commit hash for darwin-version.
+          configurationRevision = self.rev or self.dirtyRev or null;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          stateVersion = 6;
+        };
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = system;
+
+        programs.zsh.shellInit = ''
+          # Set up Nix only on SSH connections
+          # See: https://github.com/DeterminateSystems/nix-installer/pull/714
+          if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ] && [ -n "''${SSH_CONNECTION:-}" ] && [ "''${SHLVL:-0}" -eq 1 ]; then
+              . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+          fi
+          # End Nix
+          export ZDOTDIR="$HOME"/.config/zsh
+        '';
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            users.users.andouhanshirou.home = homedir;
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users."${username}" = {
+                home = {
+                  username = username;
+                  homeDirectory = homedir;
+                  # home-manager のステートバージョン
+                  stateVersion = "25.11";
+                };
               };
             };
-          };
-        }
-      ];
-    };
+          }
+        ];
+      };
 
-    # フォーマッター
-    formatter.${system} = pkgs.nixpkgs-fmt;
-  };
+      # フォーマッター
+      formatter.${system} = pkgs.nixpkgs-fmt;
+    };
 }
