bd2f9e10a5ecef643bdd5d92db9fe5ef68747152
Hosshii <sao_heath6147.wistre@icloud.com>
2026-01-25 17:07:06 +0900

    Add tmux

--- nix/home/programs/default.nix
+++ nix/home/programs/default.nix
@@ -3,6 +3,7 @@
     (import ./bat/default.nix)
     (import ./fzf/default.nix)
     (import ./git/default.nix { config = gitConfig; })
+    (import ./tmux/default.nix)
     (import ./zoxide/default.nix)
   ];
 }
