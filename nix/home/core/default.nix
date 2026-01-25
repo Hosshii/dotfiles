2694fe19baea4cdcf2dc8e2a25d87f0968cb37f9
Hosshii <sao_heath6147.wistre@icloud.com>
2026-01-25 22:29:20 +0900

    Change import

--- nix/home/core/default.nix
+++ nix/home/core/default.nix
@@ -1,13 +1,14 @@
 { gitConfig, ... }: {
   imports = [
-    (import ./bat/default.nix)
-    (import ./eza/default.nix)
-    (import ./fzf/default.nix)
+    ./bat/default.nix
+    ./eza/default.nix
+    ./fzf/default.nix
+    ./htop/default.nix
+    ./jq/default.nix
+    ./ripgrep/default.nix
+    ./tmux/default.nix
+    ./zoxide/default.nix
+
     (import ./git/default.nix { config = gitConfig; })
-    (import ./htop/default.nix)
-    (import ./jq/default.nix)
-    (import ./ripgrep/default.nix)
-    (import ./tmux/default.nix)
-    (import ./zoxide/default.nix)
   ];
 }
