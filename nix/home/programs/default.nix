cdfad15c63af29ccf69589cc1d96e5f7fd595681
Hosshii <sao_heath6147.wistre@icloud.com>
2026-01-25 17:36:34 +0900

    シンプルなプログラムをprogramsに移動

--- nix/home/programs/default.nix
+++ nix/home/programs/default.nix
@@ -1,8 +1,12 @@
 { gitConfig, ... }: {
   imports = [
     (import ./bat/default.nix)
+    (import ./eza/default.nix)
     (import ./fzf/default.nix)
     (import ./git/default.nix { config = gitConfig; })
+    (import ./htop/default.nix)
+    (import ./jq/default.nix)
+    (import ./ripgrep/default.nix)
     (import ./tmux/default.nix)
     (import ./zoxide/default.nix)
   ];
