0fe9d13f57b94cacf0c40fdb7ad20422b294e8b9
Hosshii <sao_heath6147.wistre@icloud.com>
2026-02-08 21:15:40 +0900

    Update overlay

--- nix/hosts/macbook/default.nix
+++ nix/hosts/macbook/default.nix
@@ -4,19 +4,20 @@ let
   hostname = "andouhanshirous-MacBook-Air";
   username = "andouhanshirou";
   homedir = "/Users/${username}";
+  overlays = [
+    inputs.brew-nix.overlays.default
+    inputs.claude-code-overlay.overlays.default
+  ];
   pkgs = import inputs.nixpkgs {
-    inherit system;
+    inherit system overlays;
     config.allowUnfree = true;
-    overlays = [
-      inputs.brew-nix.overlays.default
-      inputs.claude-code-overlay.overlays.default
-    ];
   };
 in
 {
   darwinConfigurations."${hostname}" = inputs.nix-darwin.lib.darwinSystem {
     modules = [
       inputs.brew-nix.darwinModules.default
+      { nixpkgs.overlays = overlays; }
       (import ../../darwin/default.nix { inherit system username pkgs homedir; self = inputs.self; brew-nix = inputs.brew-nix; })
       inputs.home-manager.darwinModules.home-manager
       {
