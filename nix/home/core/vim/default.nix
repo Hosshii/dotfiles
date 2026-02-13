0a24cb21ea3e94b65fd05a3d1c57b94fe2b4e8f5
Hosshii <sao_heath6147.wistre@icloud.com>
2026-02-14 00:41:01 +0900

    Vim

--- nix/home/core/vim/default.nix
+++ nix/home/core/vim/default.nix
@@ -4,5 +4,14 @@
     settings = {
       number = true;
     };
+    extraConfig = ''
+      " 文字エンコーディング設定
+      set encoding=utf-8
+      set fileencodings=utf-8,iso-2022-jp,euc-jp,sjis
+      set fileformats=unix,dos,mac
+      
+      " 文字化け対策
+      set ambiwidth=double
+    '';
   };
 }
